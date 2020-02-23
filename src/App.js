0e1d730f50080780059bbc3d7cc36823437a1fe1
Alix Grillo <alix.grillo2+github@gmail.com>
2020-02-23 15:16:29 -0700

    added button images and started to combine components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import buttons from "./buttons.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    buttons
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const buttons = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ buttons });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      
+      <Wrapper>
+        <Navbar />
+        <Hero />
+        <Title>Friends List</Title>
+        {this.state.buttons.map(button => (
+          <Card
+            id={button.id}
+            key={button.id}
+            image={button.image}
+          />
+        ))}
+        <Footer />
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
